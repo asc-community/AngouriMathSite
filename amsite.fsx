d73430b7fa1527c5e20bf4a56cf4a5a7e80ea320
WhiteBlackGoose <wbg@angouri.org>
2022-07-25 23:12:21 +0300

    Update amsite.fsx

--- amsite.fsx
+++ amsite.fsx
@@ -1,3 +1,20 @@
+open System.Diagnostics
+open System
 
 let runProgram name activeDir args =
-    System.Diagnostics.Process.Start(fileName: name, activeDir: activeDir, arguments: args)
+    let startInfo = StartInfo()
+    startInfo.FileName <- name
+    startInfo.Arguments <- args
+    startInfo.WorkingDirectory <- activeDir
+    let proc = Process(startInfo)
+    proc.Start()
+    proc.WaitForExit()
+
+let git = runProgram "git"
+let dotnet = runProgram "dotnet"
+
+let cliArgs = Environment.GetCommandLineArgs() |> List.ofArray
+
+match cliArgs with
+| [ "init" ] ->
+    
